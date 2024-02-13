39c4188ec291b1875b5deef030a128df726b5fa9
natalikec <156120938+natalikec@users.noreply.github.com>
2024-02-12 22:39:56 -0500

    update

--- script.js
+++ script.js
@@ -8,17 +8,31 @@ const map = new mapboxgl.Map({
 
 // Add a data source from a GeoJSON file
 map.on('load', () => {
-    map.addSource('heatrelief-data', {
+    //after the map has loaded, the below data points will load
+    map.addSource('heatrelief-data', { //adding heat relief geojson file
         type: 'geojson',
         data: 'https://raw.githubusercontent.com/natalikec/Lab2/main/Air%20Conditioned%20and%20Cool%20Spaces.geojson'
     });
     map.addLayer({
-        'id': 'center-points',
+        'id': 'center-points', //unique id for heat relief points
         'type': 'circle',
-        'source': 'heatrelief-data',
+        'source': 'heatrelief-data', //linking to data above
         'paint': {
             'circle-radius': 5,
             'circle-color': '#007cbf'
+        }
+    })
+    map.addSource('park-data', {
+        type: 'geojson',
+        data: 'https://https://githu/Users/natalikeckes/Desktop/GGR472/Lab2/green_spaces.geojsonb.com/natalikec/Lab2/blob/7258a18130c63967bca9ac374193dbca65e82c04/green_spaces.geojsonnatalikec.github.io/Lab2/main/green_spaces.geojson'
+    });
+    map.addLayer({
+        'id': 'park-polygon',
+        'type': 'fill',
+        'source': 'park-data',
+        'paint': {
+            'fill-color': '#007cbf',
+            'fill-opacity': 0.7
         }
     })
 });
